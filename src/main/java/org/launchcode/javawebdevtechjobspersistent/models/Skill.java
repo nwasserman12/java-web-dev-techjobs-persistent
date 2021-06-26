6b22fed14608616bd713b938a3faa1f738837bf7
nwasserman12 <nickyann0823@aol.com>
2021-06-26 16:32:21 -0500

    skill and employer class created

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,26 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotNull
+    @Size(max = 500, message = "Must not exceed 500 characters")
+    private String description;
+
+    public Skill(String description){
+        this.description = description;
+    }
+    public Skill(){}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
