06ac2064a5dcc740668ce61c54ee3dd584901dc8
nwasserman12 <nickyann0823@aol.com>
2021-06-26 16:26:40 -0500

    sql connected

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,25 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Employer extends AbstractEntity {
 
+    @NotNull
+    @Size(max = 50, message = "Must not exceed 50 characters")
+    private String location;
 
+    public Employer(String location){
+        this.location = location;
+    }
+
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
