48a9c5951132e6679c7d15e0bb77d102bd830f6c
nwasserman12 <nickyann0823@aol.com>
2021-07-13 21:15:17 -0500

    Still trying to get Job to add

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -30,12 +30,10 @@ public class HomeController {
     @Autowired
     private JobRepository jobRepository;
 
-
     @RequestMapping("")
     public String index(Model model) {
-
+        model.addAttribute("jobs", jobRepository.findAll());
         model.addAttribute("title", "My Jobs");
-        model.addAttribute("job", "jobs");
         return "index";
     }
 
@@ -54,25 +52,28 @@ public class HomeController {
     public String processAddJobForm(@ModelAttribute @Valid Job newJob, @Valid Employer newEmployer,
                                     Errors errors, Model model, @RequestParam int employerId,
                                     @RequestParam List<Integer> skills) {
-
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
-            model.addAttribute(new Employer());
-            model.addAttribute("employers", employerRepository.findById(employerId));
+            model.addAttribute("employers", employerRepository.findAll());
+            model.addAttribute("skills", skillRepository.findAll());
             return "add";
         }
+        Employer employer = employerRepository.findById(employerId).orElse(new Employer());
+        newJob.setEmployer(employer);
+
         List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
         newJob.setSkills(skillObjs);
-        employerRepository.save(newEmployer);
-//        jobRepository.save(newJob);
+
+        jobRepository.save(newJob);
         return "redirect:";
     }
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
+            Job job = jobRepository.findById(jobId).orElse(new Job());
+            model.addAttribute("job", job);
 
         return "view";
     }
-
 
 }
